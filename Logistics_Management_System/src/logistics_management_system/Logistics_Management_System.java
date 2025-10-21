4302e7ad85b7fba0ece234662431c899e159b651
chanuki <chanukisemini16@gmail.com>
2025-10-22 01:34:38 +0530

    project created

---
+++ Logistics_Management_System/src/logistics_management_system/Logistics_Management_System.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logistics_management_system;
+
+/**
+ *
+ * @author chanuki
+ */
+public class Logistics_Management_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
