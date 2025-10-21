ea4a0bf8783fe86513c2414a19edc7f3bc7cc4bc
chanuki <chanukisemini16@gmail.com>
2025-10-22 02:20:55 +0530

    Add city and Show Cities Functions added

--- Logistics_Management_System/src/logistics_management_system/Logistics_Management_System.java
+++ Logistics_Management_System/src/logistics_management_system/Logistics_Management_System.java
@@ -18,9 +18,8 @@ public class Logistics_Management_System {
     static int cityCount = 0;
     static String[] cities = new String[MAX_CITY_COUNT];
 
-    
     static Scanner scan = new Scanner(System.in);
-    
+
     public static void main(String[] args) {
 
         while (true) {
@@ -42,10 +41,8 @@ public class Logistics_Management_System {
             }
         }
 
-        
     }
-    
-    
+
     //City Management 
     static void manageCities() {
 
@@ -64,13 +61,13 @@ public class Logistics_Management_System {
             scan.nextLine(); // consume newline
 
             if (c == 1) {
-                //addCity();
+                addCity();
             } else if (c == 2) {
                 //renameCity();
             } else if (c == 3) {
                 //removeCity();
             } else if (c == 4) {
-                //listCities();
+                listCities();
             } else if (c == 5) {
                 //  back to main menu
                 break;
@@ -80,5 +77,43 @@ public class Logistics_Management_System {
         }
     }
 
+    //function for add new city
+    static void addCity() {
+
+        //check whether the maximun city limit has reached or not
+        if (cityCount >= MAX_CITY_COUNT) {
+            System.out.println("Cannot added more cities. City limit reached ! ");
+            return;
+        }
+
+        System.out.print("Enter city name: ");
+        String cityName = scan.nextLine();
+
+        System.out.println("\n");
+
+        //checking the entered name exits or not
+        for (int i = 0; i < cityCount; i++) {
+            if (cities[i].toLowerCase().equals(cityName.toLowerCase())) {
+                System.out.println("City " + cityName + " is already exists !");
+                return;
+            }
+        }
+        //otherwise, add the name to the list
+        cities[cityCount++] = cityName;
+        System.out.println(cityName + " Added to the List ! ");
+    }
+
+    //function for show all cities
+    static void listCities() {
+        System.out.println("All the Cities: \n");
+        if (cityCount < 1) {
+            System.out.println("No Cities to display..");
+        }
+        for (int i = 0; i < cityCount; i++) {
+            System.out.println((i + 1) + " - " + cities[i]);
+        }
+    }
+
+ 
 
 }
